9df15239575ae9b8c45491e29990ea4b2e4f3173
Yogesh1121 <jryogesh1121@gmail.com>
2024-08-20 15:20:34 +0530

    implemented review comments

--- src/pages/antd-components/upload-file/UploadFilePage.jsx
+++ src/pages/antd-components/upload-file/UploadFilePage.jsx
@@ -14,20 +14,40 @@ import XLSXIcon from '../../../assets/images/icons/excel.png';
 // CSS
 import './UploadFilePage.scss';
 
+function UploadActions({ handleReset = () => {}, handleUpload = () => {} }) {
+  return (
+    <div className="d-flex justify-content-end gap-3">
+      <Button
+        onClick={handleReset}
+        className="btn-outline-dark btn-outline-custom"
+      >
+        Reset
+      </Button>
+      <Button onClick={handleUpload} className="btn btn-primary">
+        Import
+      </Button>
+    </div>
+  );
+}
+
+function UploadSelectedBody() {
+  return (
+    <>
+      <UploadIcon />
+      <p className="ant-upload-text fs-5 fw-semibold my-2 opacity-75">
+        Drag & Drop or <span className="text-primary">Choose file</span> to
+        upload
+      </p>
+      <p className="ant-upload-hint m-0">File Type : XLSX, CSV</p>
+    </>
+  );
+}
+
 function UploadDraggerBody({ isFileSelected = false, file = {} }) {
   const { name } = file;
 
   if (!isFileSelected) {
-    return (
-      <>
-        <UploadIcon />
-        <p className="ant-upload-text fs-5 fw-semibold my-2 opacity-75">
-          Drag & Drop or <span className="text-primary">Choose file</span> to
-          upload
-        </p>
-        <p className="ant-upload-hint m-0">File Type : XLSX, CSV</p>
-      </>
-    );
+    return <UploadSelectedBody />;
   }
   return (
     <>
@@ -94,17 +114,7 @@ export default function UploadFilePage() {
           <UploadDraggerBody isFileSelected={isFileSelected} file={file} />
         </Upload.Dragger>
       </div>
-      <div className="d-flex justify-content-end gap-3">
-        <Button
-          onClick={handleReset}
-          className="btn-outline-dark btn-outline-custom"
-        >
-          Reset
-        </Button>
-        <Button onClick={handleUpload} className="btn btn-primary">
-          Import
-        </Button>
-      </div>
+      <UploadActions handleReset={handleReset} handleUpload={handleUpload} />
     </div>
   );
 }
