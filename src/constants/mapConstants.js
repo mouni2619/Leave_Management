4221f50695cfb9ae90de18e4eb93df1b3980d885
Yogesh1121 <jryogesh1121@gmail.com>
2024-08-20 23:28:42 +0530

    moved pointer icon url and size to constant

--- src/constants/mapConstants.js
+++ src/constants/mapConstants.js
@@ -3,3 +3,9 @@ export const MAP_TILE_DETAILS = {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 };
+
+export const MAP_POINTER = {
+  iconUrl:
+    'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW1hcC1waW4iPjxwYXRoIGQ9Ik0yMCAxMGMwIDQuOTkzLTUuNTM5IDEwLjE5My03LjM5OSAxMS43OTlhMSAxIDAgMCAxLTEuMjAyIDBDOS41MzkgMjAuMTkzIDQgMTQuOTkzIDQgMTBhOCA4IDAgMCAxIDE2IDAiLz48Y2lyY2xlIGN4PSIxMiIgY3k9IjEwIiByPSIzIi8+PC9zdmc+',
+  iconSize: [30, 30],
+};
--- src/pages/maps/NormalMapPage.jsx
+++ src/pages/maps/NormalMapPage.jsx
@@ -1,6 +1,9 @@
 import { Icon } from 'leaflet';
 import { Marker, Popup } from 'react-leaflet';
 
+// Constants
+import { MAP_POINTER } from '../../constants/mapConstants';
+
 // Components
 import LLMap from '../../components/maps/LLMap';
 
@@ -10,9 +13,8 @@ export default function NormalMapPage() {
 
   // custom icon for the pointer marker
   const customIcon = new Icon({
-    iconUrl:
-      'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLW1hcC1waW4iPjxwYXRoIGQ9Ik0yMCAxMGMwIDQuOTkzLTUuNTM5IDEwLjE5My03LjM5OSAxMS43OTlhMSAxIDAgMCAxLTEuMjAyIDBDOS41MzkgMjAuMTkzIDQgMTQuOTkzIDQgMTBhOCA4IDAgMCAxIDE2IDAiLz48Y2lyY2xlIGN4PSIxMiIgY3k9IjEwIiByPSIzIi8+PC9zdmc+',
-    iconSize: [30, 30],
+    iconUrl: MAP_POINTER.iconUrl,
+    iconSize: MAP_POINTER.iconSize,
   });
   return (
     <div className="page-content">
